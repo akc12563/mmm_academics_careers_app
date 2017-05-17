264c4f58a8f600f5579bc0467787839d36d6c0f3
Raghu Betina <raghu@firstdraft.com>
2017-05-17 18:18:52 +0000

    Whitelist columns in Job for ActiveAdmin

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Job do
 
+ permit_params :company, :position, :location, :job_description, :job_requirements, :job_link, :contact_name, :contact_email, :contact_phone, :deadline
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
